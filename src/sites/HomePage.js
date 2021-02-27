0ea3cba4dac1131cd6670cdc5a2ccd9850c85dd2
Sören Michaels <soeren.michaels@gmail.com>
2021-02-27 23:49:53 +0100

    addEd Funky Graph

--- src/sites/HomePage.js
+++ src/sites/HomePage.js
@@ -1,9 +1,46 @@
 import Title from 'antd/lib/typography/Title';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar } from 'recharts';
 
 const data = [{name: 'Test A', uv: 400, pv: 2400, amt: 2400} ];
 
-
+const data2 = [
+  {
+    subject: 'Math',
+    A: 120,
+    B: 110,
+    fullMark: 150,
+  },
+  {
+    subject: 'Chinese',
+    A: 98,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'English',
+    A: 86,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'Geography',
+    A: 99,
+    B: 100,
+    fullMark: 150,
+  },
+  {
+    subject: 'Physics',
+    A: 85,
+    B: 90,
+    fullMark: 150,
+  },
+  {
+    subject: 'History',
+    A: 65,
+    B: 85,
+    fullMark: 150,
+  },
+];
 export default function HomePage() {
   return <>
   <Title level="1">Cooler Graph</Title>
@@ -13,5 +50,13 @@ export default function HomePage() {
     <XAxis dataKey="name" />
     <YAxis />
     <Tooltip />
-  </LineChart></>;
+    </LineChart>
+  <ResponsiveContainer width="100%" height="100%">
+  <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data2}>
+    <PolarGrid />
+    <PolarAngleAxis dataKey="subject" />
+    <PolarRadiusAxis />
+    <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+  </RadarChart>
+</ResponsiveContainer></>;
 }
