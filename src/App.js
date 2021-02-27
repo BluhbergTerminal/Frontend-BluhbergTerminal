026547c4791a90830b15877218add85a547c545d
s0er3n <48496757+s0er3n@users.noreply.github.com>
2021-02-27 20:10:22 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Navbar from "./components/Navbar";
 import "./styles.css";
+import { useRoutes } from "hookrouter";
+import HomePage from "./sites/HomePage";
+import NotFoundPage from "./sites/NotFoundPage";
+
+const routes = {
+  "/": () => <HomePage />
+};
 export default function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+  const routeResult = useRoutes(routes);
+
+  return routeResult || <NotFoundPage />;
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import { Menu } from "antd";
 import "antd/dist/antd.css";
 import { Layout } from "antd";
+import { navigate } from "hookrouter";
 
 export default function Navbar() {
   return (
     <>
       <Layout.Header>
         <Menu>
-          <Menu.Item key="1">Test</Menu.Item>
+          <Menu.Item key="1" onClick={() => navigate("/test")}>
+            Test
+          </Menu.Item>
         </Menu>
       </Layout.Header>
     </>
