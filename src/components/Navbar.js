af5011ec65938818f0abc5f69da7bee5934c2e52
s0er3n <soeren.michaels@gmail.com>
2021-02-27 20:45:26 +0100

    removed ReactStrictMode to make it compatible to hookrouter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import { Menu } from "antd";
 import "antd/dist/antd.css";
 import { Layout } from "antd";
-import { navigate } from "hookrouter";
+import { A } from "hookrouter";
 
 export default function Navbar() {
   return (
     <>
       <Layout.Header>
         <Menu>
-          <Menu.Item key="1" onClick={() => navigate("/")}>
-            Test
+          <Menu.Item key="1" >
+            <A href="/">Test</A>
           </Menu.Item>
         </Menu>
       </Layout.Header>
